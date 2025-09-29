caf612072c8f8835bee7b9cc5249e248c8b0fa73
gavinchou94 <gaz11@pitt.edu>
2025-09-29 08:38:09 -0400

    add problem05 starter code

---
+++ Problems/problem05/solver.hpp
@@ -0,0 +1,60 @@
+#ifndef _SOLVER_HPP
+#define _SOLVER_HPP
+
+#include <cstdlib>
+
+#include "constants.hpp"
+
+void matrix_vector_product(const double matrix[][CAP],
+                           const double vector[],
+                           double result[], const int dim)
+{
+    // loop over rows of matrix
+    for (int i = 0; i < dim; i++)
+    {
+        // initialize product element for this row
+        result[i] = 0;
+        // loop over columns of matrix
+        for (int j = 0; j < dim; j++)
+        {
+            result[i] += matrix[i][j] * vector[j];
+        }
+    }
+}
+
+void submatrix(const double matrix[][CAP],
+               int row, int col,
+               double result[][CAP], const int dim)
+{
+    // TODO
+}
+
+double determinant(const double matrix[][CAP], const int dim)
+{
+    // TODO
+    return 0;
+}
+
+void replace_column(const double matrix[][CAP],
+                    int col, const double vector[],
+                    double result[][CAP], const int dim)
+{
+    // TODO
+}
+
+void cramers_rule(const double matrix[][CAP],
+                  const double vector[],
+                  double result[], const int dim)
+{
+    // TODO
+}
+
+int parse_file_input(std::string filename,
+                     double matrix[][CAP],
+                     double vector[])
+{
+    // BONUS TODO
+    return 0;
+}
+
+#endif
