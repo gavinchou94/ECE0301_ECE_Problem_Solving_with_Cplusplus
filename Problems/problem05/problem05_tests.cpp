8cd44bfcaecb4b7c3ea3a509f82c698936b1c563
gavinchou94 <gaz11@pitt.edu>
2025-10-21 07:17:43 -0400

    minor corrections on folder/file names

--- Problems/problem05/problem05_tests.cpp
+++ Problems/problem05/problem05_tests.cpp
@@ -138,5 +138,200 @@ TEST_CASE("Test cramer's rule dim 2", "[solver]")
         REQUIRE(abs(result[i] - test[i]) <= THRESHOLD);
     }
 }
-
 /* Write your own test cases */
+TEST_CASE("Test multiple submatrix dim 3", "[submatrix]")
+{
+    const int dim = 3;
+    int row = 0, col = 0;
+    double matrix[dim][CAP] = {{2, 1, 1}, {1, -1, -1}, {1, 2, 1}};
+    double result[dim][CAP];
+    double test[dim][CAP] = {{-1, -1}, {2, 1}};
+    submatrix(matrix, row, col, result, dim);
+
+    for (int i = 0; i < dim - 1; i++)
+    {
+        for (int j = 0; j < dim - 1; j++)
+        {
+            REQUIRE(abs(result[i][j] - test[i][j]) <= THRESHOLD);
+        }
+    }
+}
+TEST_CASE("Test multiple submatrix dim 4", "[submatrix]")
+{
+    const int dim = 4;
+    int row = 1, col = 2;
+    double matrix[dim][CAP] = {{0, 10, 4, -2},
+                               {-3, -17, 1, 2},
+                               {1, 1, 1, 0},
+                               {8, -34, 16, -10}};
+    double result[dim][CAP];
+    double test[dim][CAP] = {{0, 10, -2}, {1, 1, 0}, {8, -34, -10}};
+    submatrix(matrix, row, col, result, dim);
+
+    for (int i = 0; i < dim - 1; i++)
+    {
+        for (int j = 0; j < dim - 1; j++)
+        {
+            REQUIRE(abs(result[i][j] - test[i][j]) <= THRESHOLD);
+        }
+    }
+}
+TEST_CASE("Test multiple submatrix dim 5", "[submatrix]")
+{
+    const int dim = 5;
+    int row = 0, col = 0;
+    double matrix[dim][CAP] = {{1, 1, 0, 1, 0},
+                               {1, 0, 1, 0, 1},
+                               {0, 300, 0, -300, 0},
+                               {0, 0, 150, 0, -150},
+                               {45000, -22500, -45000, 0, 0}};
+    double result[dim][CAP];
+    double test[dim][CAP] = {{0, 1, 0, 1},
+                             {300, 0, -300, 0},
+                             {0, 150, 0, -150},
+                             {-22500, -45000, 0, 0}};
+    submatrix(matrix, row, col, result, dim);
+
+    for (int i = 0; i < dim - 1; i++)
+    {
+        for (int j = 0; j < dim - 1; j++)
+        {
+            REQUIRE(abs(result[i][j] - test[i][j]) <= THRESHOLD);
+        }
+    }
+}
+TEST_CASE("Test determinant |A| = -3.32, n = 2", "[determinant]")
+{
+    const int dim = 2;
+    double matrix[dim][CAP] = {{0.4, 1.2}, {1.7, -3.2}};
+    double test = -3.32;
+    double result = determinant(matrix, dim);
+
+    REQUIRE(abs(result - test) <= THRESHOLD);
+}
+TEST_CASE("Test determinant |A| = 3, n = 3", "[determinant]")
+{
+    const int dim = 3;
+    double matrix[dim][CAP] = {{2, 1, 1}, {1, -1, -1}, {1, 2, 1}};
+    double test = 3.0;
+    double result = determinant(matrix, dim);
+
+    REQUIRE(abs(result - test) <= THRESHOLD);
+}
+TEST_CASE("Test determinant |A| = -1568, n = 4", "[determinant]")
+{
+    const int dim = 4;
+    double matrix[dim][CAP] = {{0, 10, 4, -2},
+                               {-3, -17, 1, 2},
+                               {1, 1, 1, 0},
+                               {8, -34, 16, -10}};
+    double test = -1568;
+    double result = determinant(matrix, dim);
+
+    REQUIRE(abs(result - test) <= THRESHOLD);
+}
+TEST_CASE("Test determinant |A| = 1.4175*10^10, n = 5", "[determinant]")
+{
+    const int dim = 5;
+    double matrix[dim][CAP] = {{1, 1, 0, 1, 0},
+                               {1, 0, 1, 0, 1},
+                               {0, 300, 0, -300, 0},
+                               {0, 0, 150, 0, -150},
+                               {45000, -22500, -45000, 0, 0}};
+    double test = 1.4175 * pow(10, 10);
+    double result = determinant(matrix, dim);
+
+    REQUIRE(abs(result - test) <= THRESHOLD);
+}
+TEST_CASE("Test col replace dim 3", "[replace_column]")
+{
+    const int dim = 3;
+    double matrix[dim][CAP] = {{2, 1, 1}, {1, -1, -1}, {1, 2, 1}};
+    double vector[dim] = {1, -2, 3};
+    double result[dim][CAP];
+    int col = 1;
+    double test[dim][CAP] = {{2, 1, 1}, {1, -2, -1}, {1, 3, 1}};
+    replace_column(matrix, col, vector, result, dim);
+
+    for (int i = 0; i < dim; i++)
+    {
+        for (int j = 0; j < dim; j++)
+        {
+            REQUIRE(abs(result[i][j] - test[i][j]) <= THRESHOLD);
+        }
+    }
+}
+TEST_CASE("Test col replace dim 4", "[replace_column]")
+{
+    const int dim = 4;
+    double matrix[dim][CAP] = {{0, 10, 4, -2},
+                               {-3, -17, 1, 2},
+                               {1, 1, 1, 0},
+                               {8, -34, 16, -10}};
+    double vector[dim] = {4, 0, 2, 6};
+    double result[dim][CAP];
+    int col = 3;
+    double test[dim][CAP] = {{0, 10, 4, 4},
+                             {-3, -17, 1, 0},
+                             {1, 1, 1, 2},
+                             {8, -34, 16, 6}};
+    replace_column(matrix, col, vector, result, dim);
+
+    for (int i = 0; i < dim; i++)
+    {
+        for (int j = 0; j < dim; j++)
+        {
+            REQUIRE(abs(result[i][j] - test[i][j]) <= THRESHOLD);
+        }
+    }
+}
+// I used https://matrix.reshish.com/cramer/ to find my vector[dim] and test[dim] for the Cramer's rule
+TEST_CASE("Test cramer's rule dim 3", "[cramers_rule]")
+{
+    const int dim = 3;
+    double matrix[dim][CAP] = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+    double vector[dim] = {1, 2, 3};
+    double result[dim];
+    double test[dim] = {1, 2, 3};
+    cramers_rule(matrix, vector, result, dim);
+
+    for (int i = 0; i < dim; i++)
+    {
+        REQUIRE(abs(result[i] - test[i]) <= THRESHOLD);
+    }
+}
+TEST_CASE("Test cramer's rule dim 4", "[cramers_rule]")
+{
+    const int dim = 4;
+    double matrix[dim][CAP] = {{0, 10, 4, -2},
+                               {-3, -17, 1, 2},
+                               {1, 1, 1, 0},
+                               {8, -34, 16, -10}};
+    double vector[dim] = {12, -17, 3, -20};
+    double result[dim];
+    double test[dim] = {1, 1, 1, 1};
+    cramers_rule(matrix, vector, result, dim);
+
+    for (int i = 0; i < dim; i++)
+    {
+        REQUIRE(abs(result[i] - test[i]) <= THRESHOLD);
+    }
+}
+TEST_CASE("Test cramer's rule dim 5", "[cramers_rule]")
+{
+    const int dim = 5;
+    double matrix[dim][CAP] = {{1, 1, 0, 1, 0},
+                               {1, 0, 1, 0, 1},
+                               {0, 300, 0, -300, 0},
+                               {0, 0, 150, 0, -150},
+                               {45000, -22500, -45000, 0, 0}};
+    double vector[dim] = {-5, 9, 600, -300, -45000};
+    double result[dim];
+    double test[dim] = {1, -2, 3, -4, 5};
+    cramers_rule(matrix, vector, result, dim);
+
+    for (int i = 0; i < dim; i++)
+    {
+        REQUIRE(abs(result[i] - test[i]) <= THRESHOLD);
+    }
+}
--- Problems/problem05/solver.hpp
+++ Problems/problem05/solver.hpp
@@ -27,12 +27,48 @@ void submatrix(const double matrix[][CAP],
                double result[][CAP], const int dim)
 {
     // TODO
+    int subrow = 0;
+    for (int i = 0; i < dim; i++)
+    {
+        if (i != row)
+        {
+            int subcol = 0;
+            for (int j = 0; j < dim; j++)
+            {
+                if (j != col)
+                {
+                    result[subrow][subcol] = matrix[i][j];
+                    subcol++;
+                }
+            }
+            subrow++;
+        }
+    }
 }
-
+// I've used // I used https://matrix.reshish.com/ to find det
 double determinant(const double matrix[][CAP], const int dim)
 {
     // TODO
-    return 0;
+    if (dim == 0)
+    { // Check invalid matrix
+        return 0;
+    }
+    if (dim == 1)
+    { // 1x1 matrix
+        return matrix[0][0];
+    }
+    double det = 0;
+    double sub[dim][CAP]; // Temp submatrix
+    int sign = 1;         // Sign alternator
+
+    for (int j = 0; j < dim; j++)
+    {
+        submatrix(matrix, 0, j, sub, dim);
+        double subdet = determinant(sub, dim - 1);
+        det += sign * matrix[0][j] * subdet;
+        sign = -sign; // Flip sign for the next integer
+    }
+    return det;
 }
 
 void replace_column(const double matrix[][CAP],
@@ -40,13 +76,37 @@ void replace_column(const double matrix[][CAP],
                     double result[][CAP], const int dim)
 {
     // TODO
+    for (int i = 0; i < dim; i++)
+    {
+        for (int j = 0; j < dim; j++)
+        {
+            if (j == col)
+            {
+                result[i][j] = vector[i];
+            }
+            else
+            {
+                result[i][j] = matrix[i][j];
+            }
+        }
+    }
 }
-
 void cramers_rule(const double matrix[][CAP],
                   const double vector[],
                   double result[], const int dim)
 {
     // TODO
+    double det = determinant(matrix, dim);
+    if (det == 0)
+    { // Crammer's rule cannot be applied
+        return;
+    }
+    double modmat[dim][CAP]; // Modified matrix
+    for (int j = 0; j < dim; j++)
+    {
+        replace_column(matrix, j, vector, modmat, dim);
+        result[j] = determinant(modmat, dim) / det;
+    }
 }
 
 int parse_file_input(std::string filename,
