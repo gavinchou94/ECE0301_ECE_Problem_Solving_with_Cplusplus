04034ff4a337ebd3ede3978ebbf8d2f75e9d80c0
gavinchou94 <gaz11@pitt.edu>
2025-11-10 22:28:46 -0500

    add more comments in Lecture 9 codes, adding exam 2

---
+++ Problems/exam2/Exam2.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <stdexcept>
+#include <algorithm>
+#include <vector>
+
+#include "Exam2.hpp"
+
+void SampleClass::displayMessage() const
+{
+  std::cout << "Object-oriented programming rocks!" << std::endl;
+}
