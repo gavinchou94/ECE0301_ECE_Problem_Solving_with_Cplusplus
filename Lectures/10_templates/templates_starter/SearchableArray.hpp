ffcb93cca29636efdf01be703250d56338413093
gavinchou94 <gaz11@pitt.edu>
2025-11-17 09:45:28 -0500

    add lecture 10 templates starter code

---
+++ Lectures/10_templates/templates_starter/SearchableArray.hpp
@@ -0,0 +1,13 @@
+#ifndef _SEARCHABLE_ARRAY_HPP
+#define _SEARCHABLE_ARRAY_HPP
+
+#include "SafeArray.hpp"
+
+class SearchableArray : public SafeArray
+{
+public:
+    using SafeArray::SafeArray;
+    bool search(int);
+};
+
+#endif
